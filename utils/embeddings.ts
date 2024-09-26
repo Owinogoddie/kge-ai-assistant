1502b1fad423f935b82131b733226e60c47e7b24
Owinogoddie <owinogoddie@gmail.com>
2024-09-26 10:19:13 +0000

    updating embeddings

--- utils/embeddings.ts
+++ utils/embeddings.ts
@@ -1,16 +1,10 @@
 import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { TaskType } from "@google/generative-ai";
-let embeddings;
 
-try {
-  embeddings = new GoogleGenerativeAIEmbeddings({
+export const googleEmbeddings = new GoogleGenerativeAIEmbeddings({
     apiKey: process.env.GOOGLE_API_KEY,
     model: "embedding-001",
     taskType: TaskType.RETRIEVAL_DOCUMENT,
   });
-  console.log('embedings exists')
-} catch (error) {
-  console.error("Error creating Google Embeddings:", error);
-}
 
-export const googleEmbeddings = embeddings;
+  
